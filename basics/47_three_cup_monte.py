be3f77f7cc76dcf73f912425cf025801c4154fc6
Carl S <sinocruc@oregonstate.edu>
2021-01-22 09:11:39 -0800

    basic user input/ user input validation

---
+++ basics/47_three_cup_monte.py
@@ -0,0 +1,39 @@
+#Udemy - 2021 Complete Python Bootcamp From Zero to Hero in Python
+#Lecture 47
+#Compartmentalizing functions in a program
+from random import shuffle
+
+def shuffle_list(game_cups):
+    shuffle(game_cups)
+    return game_cups
+
+def player_guess():
+    options = ['0', '1', '2']
+    guess = ''
+    while guess not in options:
+        guess = input("Pick your cup (0, 1, or 2): ")
+    #user input always returned as string, so you need to cast it
+    return int(guess)  
+
+def check_guess(cups, players_cup):
+    if cups[players_cup] == '0':
+        print("You guessed right!")
+    else:
+        print("Sorry, maybe next time!")
+    print(cups)
+
+#Initialize list
+cups = ['0', ' ', ' ']
+
+#Shuffle list
+shuffled_cups = shuffle_list(cups)
+
+#User guess
+players_cup = player_guess()
+
+#Checks user guess
+check_guess(shuffled_cups, players_cup)
+
+
+
+
